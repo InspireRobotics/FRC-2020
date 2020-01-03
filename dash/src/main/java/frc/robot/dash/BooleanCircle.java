88d532376027085ce5fa29b0f9d91fed5c0de704
DevOrc <ncharlton002@gmail.com>
2020-01-02 19:15:45 -0500

    Add dash module
    This adds the dashboard module to the project. The dashboard module is
    set up to build a plugin for Shuffleboard. This will allow us to create
    custom widgets on the dashboard.

---
+++ dash/src/main/java/frc/robot/dash/BooleanCircle.java
@@ -0,0 +1,90 @@
+package frc.robot.dash;
+
+import java.util.List;
+import java.util.Arrays;
+
+import edu.wpi.first.shuffleboard.api.prefs.Group;
+import edu.wpi.first.shuffleboard.api.prefs.Setting;
+import edu.wpi.first.shuffleboard.api.widget.Description;
+import edu.wpi.first.shuffleboard.api.widget.ParametrizedController;
+import edu.wpi.first.shuffleboard.api.widget.SimpleAnnotatedWidget;
+
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.Property;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.fxml.FXML;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+
+@Description(name = "Boolean Circle", dataTypes = Boolean.class)
+@ParametrizedController("BooleanCircleWidget.fxml") 
+public class BooleanCircle extends SimpleAnnotatedWidget<Boolean> {
+
+    @FXML
+    private Pane root;
+
+    private final Property<Color> trueColor = new SimpleObjectProperty<>(this, "colorWhenTrue", Color.LAWNGREEN);
+    private final Property<Color> falseColor = new SimpleObjectProperty<>(this, "colorWhenFalse", Color.DARKRED);
+
+    @FXML
+    private void initialize() {
+        root.backgroundProperty().bind(Bindings.createObjectBinding(() -> createSolidColorBackground(getColor()),
+                dataProperty(), trueColor, falseColor));
+    }
+
+    @Override
+    public List<Group> getSettings() {
+        return Arrays.asList(Group.of("Colors",
+                Setting.of("Color when true", "The color to use when the value is `true`", trueColor, Color.class),
+                Setting.of("Color when false", "The color to use when the value is `false`", falseColor, Color.class)));
+    }
+
+    @Override
+    public Pane getView() {
+        return root;
+    }
+
+    private Color getColor() {
+        final Boolean data = getData();
+        if (data == null) {
+            return Color.ORANGE;
+        }
+
+        if (data) {
+            return trueColor.getValue();
+        } else {
+            return falseColor.getValue();
+        }
+    }
+
+    private Background createSolidColorBackground(Color color) {
+        return new Background(new BackgroundFill(color, new CornerRadii(25, true), null));
+    }
+
+    public Color getTrueColor() {
+        return trueColor.getValue();
+    }
+
+    public Property<Color> trueColorProperty() {
+        return trueColor;
+    }
+
+    public void setTrueColor(Color trueColor) {
+        this.trueColor.setValue(trueColor);
+    }
+
+    public Color getFalseColor() {
+        return falseColor.getValue();
+    }
+
+    public Property<Color> falseColorProperty() {
+        return falseColor;
+    }
+
+    public void setFalseColor(Color falseColor) {
+        this.falseColor.setValue(falseColor);
+    }
+}
---
+++ dash/src/main/java/frc/robot/dash/Dash.java
@@ -0,0 +1,30 @@
+package frc.robot.dash;
+
+import edu.wpi.first.shuffleboard.api.widget.ComponentType;
+import edu.wpi.first.shuffleboard.api.widget.WidgetType;
+
+import java.util.Arrays;
+import java.util.List;
+
+import edu.wpi.first.shuffleboard.api.plugin.Description;
+import edu.wpi.first.shuffleboard.api.plugin.Plugin;;
+
+// @Requires(group = "edu.wpi.first.shuffleboard", name = "Base", minVersion = "1.0.0")
+@Description(group = "org.inspirerobotics.frc2020", name = "Dash", version = "0.1.0", summary = "FRC 4283 Custom Plugin")
+public class Dash extends Plugin {
+
+    @Override
+    public void onLoad() {
+        System.out.println("Dash loaded");
+    }
+
+    @Override
+    public void onUnload() {
+        System.out.println("Dash unloaded");
+    }
+
+    @Override
+    public List<ComponentType> getComponents() {
+        return Arrays.asList(WidgetType.forAnnotatedWidget(BooleanCircle.class));
+    }
+}
